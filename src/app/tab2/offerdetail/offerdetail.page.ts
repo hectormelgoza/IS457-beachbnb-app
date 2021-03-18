0bbae781745071b6f2f67f0d1915a34c9652161b
Hector Melgoza <h.melgoza.4@gmail.com>
2021-03-18 11:10:29 -0700

    initial offerdetail page

---
+++ src/app/tab2/offerdetail/offerdetail.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Place } from 'src/app/tab1/place.model';
+import { PlaceService } from 'src/app/tab1/place.service';
+
+@Component({
+  selector: 'app-offerdetail',
+  templateUrl: './offerdetail.page.html',
+  styleUrls: ['./offerdetail.page.scss'],
+})
+export class OfferdetailPage implements OnInit {
+  place: Place;
+  id: string;
+
+  constructor(private route: ActivatedRoute,
+    private placeService: PlaceService) { }
+
+  ngOnInit() {
+    this.id = this.route.snapshot.paramMap.get('placeId');
+    this.place = this.placeService.getPlace(this.id);
+    console.log(this.place)
+  }
+
+}

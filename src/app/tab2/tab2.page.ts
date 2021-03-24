e9b15d310e167b30a647a421c9e1a25f8bd84469
Hector Melgoza <h.melgoza.4@gmail.com>
2021-03-23 21:49:35 -0700

    edit offer

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActionSheetController } from '@ionic/angular';
 import { Place } from '../tab1/place.model';
 import { PlaceService } from '../tab1/place.service';
 
@@ -10,11 +12,48 @@ import { PlaceService } from '../tab1/place.service';
 export class Tab2Page {
   offers: Place[];
 
-  constructor(private placeService: PlaceService) {}
+  constructor(private placeService: PlaceService,
+    private actionSheetController: ActionSheetController,
+    private router: Router) {}
 
   ionViewWillEnter(){
     this.offers = this.placeService.places;
     console.log(this.offers);
+  }
+  async onClickOffer(id: string){
+    const actionSheet = await this.actionSheetController.create({
+      header: 'You would like to:',
+      buttons: [
+        {
+          text: 'Remove Offer',
+          icon: 'trash',
+          handler: () => {
+            this.placeService.removeOffer(id);
+            this.offers = this.placeService.places;
+          }
+        },
+        {
+          text: 'View Detail',
+          icon: 'eye',
+          handler: () => {
+            this.router.navigate(['/tabs/tab2/offerdetail/'+id]);
+          }
+        },
+        {
+          text: 'Edit Offer',
+          icon: 'pencil',
+          handler: () => {
+            this.router.navigate(['/tabs/tab2/editoffer/'+id]);
+          }
+        },
+        {
+          text: 'Cancel',
+          icon: 'close',
+          role: 'cancel'
+        }
+      ]
+    });
+    await actionSheet.present();
   }
 
 }
